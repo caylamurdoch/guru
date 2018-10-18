505f63f1ed6867b94d836fb1434879e3a64b78b8
Cayla Murdoch <cayla.murdoch@gmail.com>
2018-10-18 20:17:20 +0000

    basic html and js allowing user to send/display messages

---
+++ guru.js
@@ -0,0 +1,13 @@
+angular.module('guru', [])
+    .controller('guruCtrl', guruCtrl)
+
+function guruCtrl($scope) {
+    $scope.messages = [];
+
+    $scope.sendMessage = function(message) {
+        $scope.messages.push({
+            text: message.text
+        });
+        message.text = ''
+    };
+}
