398eb1e7a235f8e413308bab2a84d35556363550
Cayla Murdoch <cayla.murdoch@gmail.com>
2018-10-19 05:30:51 +0000

    speech bubbles

--- guru.js
+++ guru.js
@@ -1,5 +1,6 @@
 angular.module('guru', [])
     .controller('guruCtrl', guruCtrl)
+    .directive('conversation', conversationDirective);
 
 
 var stashedComments = (function() {
@@ -16,37 +17,45 @@ var stashedComments = (function() {
     return json;
 })();
 
-var comments_json = stashedComments
-
-console.log(comments_json)
-console.log(comments_json["allData"][0])
-
-function consultGuru(userReply) {
-    return getRandomReply()
-}
-
-function getRandomReply() {
-
-    let index = Math.floor(Math.random() * (comments_json["allData"].length - 1));
-    console.log(index)
-    return comments_json["allData"][index]
-
-}
+var comments_json = stashedComments;
+var index = 0;
 
 function guruCtrl($scope) {
     $scope.messages = [];
 
     $scope.sendMessage = function(message) {
         $scope.messages.push({
-            text: message.text
+            text: message.text,
+            response: consultGuru(message.text),
+            number: index
         });
 
-        $scope.messages.push({
-            text: consultGuru(message.text)
-        });
-
+        index++;
         message.text = '';
+        console.log($scope.messages)
     };
 
+    function consultGuru(userReply) {
+        return getRandomReply()
+    }
 
+    function getRandomReply() {
+        let index = Math.floor(Math.random() * (comments_json["allData"].length - 1));
+        return comments_json["allData"][index]
+    }
+}
+
+function conversationDirective() {
+    return {
+        scope: {
+            content: '='
+        },
+        restrict: 'E',
+        replace: 'true',
+        template: (
+            '<div class="speech-bubble">' +
+            '{{content}}' +
+            '</div>'
+        ),
+    };
 }
