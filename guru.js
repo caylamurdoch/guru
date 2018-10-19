f10d05673128ae1ef9b3e98d717d4a1d413879f3
murdo25 <murdoch@byu.edu>
2018-10-19 04:41:09 +0000

    stuff

--- guru.js
+++ guru.js
@@ -10,14 +10,12 @@ var stashedComments = (function() {
         'url': "data.2.json",
         'dataType': "json",
         'success': function(data) {
-            // console.log("success data:", data)
             json = data;
         }
     });
     return json;
 })();
 
-// var comments_json = JSON.parse('data.2.json');
 var comments_json = stashedComments
 
 console.log(comments_json)
