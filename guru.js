35a1e3b5f7375ae9b19090568e84ea46bf0b3348
murdo25 <murdoch@byu.edu>
2018-10-19 04:17:17 +0000

    added data

--- guru.js
+++ guru.js
@@ -1,6 +1,34 @@
 angular.module('guru', [])
     .controller('guruCtrl', guruCtrl)
 
+
+var stashedComments = (function() {
+    var json = null;
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url': "data.2.json",
+        'dataType': "json",
+        'success': function(data) {
+            // console.log("success data:", data)
+            json = data;
+        }
+    });
+    return json;
+})();
+
+// var comments_json = JSON.parse('data.2.json');
+var comments_json = stashedComments
+
+console.log(comments_json)
+console.log(comments_json["allData"][0])
+
+
+
+function getRandomReply() {
+
+}
+
 function guruCtrl($scope) {
     $scope.messages = [];
 
@@ -10,4 +38,6 @@ function guruCtrl($scope) {
         });
         message.text = '';
     };
+
+
 }
